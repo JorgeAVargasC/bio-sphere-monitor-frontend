a9677da849eb9acd31f2d7af9f64829503dcae75
JorgeAVargasC <javargas1209@gmail.com>
2025-01-19 13:44:22 -0500

    fix: only json data

--- src/app/dashboard/shared/hooks/use-get-beehives-data.tsx
+++ src/app/dashboard/shared/hooks/use-get-beehives-data.tsx
@@ -1,22 +1,26 @@
-import { useEffect, useState } from 'react'
-import { ref, onValue, off } from 'firebase/database'
-import { realtimeDB } from '../../../../firebase/firebase.config'
+import {
+  //  useEffect,
+  useState
+} from 'react'
+// import { ref, onValue, off } from 'firebase/database'
+// import { realtimeDB } from '../../../../firebase/firebase.config'
 import { IFirebaseData } from '@dashboard/beehives/components/cards/interfaces'
 import jsonData from '../data/updated_beehive_data.json'
 
 export const useGetBeehivesData = () => {
-  const [data, setData] = useState<IFirebaseData | null>(null)
+  const [data] = useState<IFirebaseData | null>(jsonData)
 
-  useEffect(() => {
-    const dataRef = ref(realtimeDB)
+  // * Realtime DB
+  // useEffect(() => {
+  //   const dataRef = ref(realtimeDB)
 
-    const unsubscribe = onValue(dataRef, (snapshot) => {
-      const data = snapshot.val()
-      setData(data as IFirebaseData)
-    })
+  //   const unsubscribe = onValue(dataRef, (snapshot) => {
+  //     const data = snapshot.val()
+  //     setData(data as IFirebaseData)
+  //   })
 
-    return () => off(dataRef, 'value', unsubscribe)
-  }, [])
+  //   return () => off(dataRef, 'value', unsubscribe)
+  // }, [])
 
   return data ?? jsonData
 }
